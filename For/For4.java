793b35d855a3d33a89647ff867d1eaa3cfcbd8a6
IsmailovOgabek <74254825+IsmailovOgabek@users.noreply.github.com>
2022-10-08 11:21:03 +0500

    Javadan masalalar

--- For/For4.java
+++ For/For4.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class For4 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.print("1 kg konfetning narxinikiriting = ");
+        System.out.print("1 kg konfetning narxinikiriting =  ");
         int narx = in.nextInt();
         for (int i = 1; i <=10 ; i++) {
             System.out.println(i+" "+narx*i);
